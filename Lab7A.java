0f4f34e2ad94febcd6db046694b531f9087be4b8
MysticMan1 <44099131+MysticMan1@users.noreply.github.com>
2018-10-12 13:11:50 -0700

    Add files via upload

---
+++ Lab7A.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab7A;
+
+/**
+ *
+ * @author Ahssan
+ */
+//Hi my name is Ahssan Khan
+public class Lab7A {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
